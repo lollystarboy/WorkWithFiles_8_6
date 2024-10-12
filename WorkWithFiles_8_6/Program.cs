b4f15a206a1c864b4a70240579088b4c2ca81a84
cherneckiy <deneff.max@yandex.ru>
2024-10-12 17:33:16 +0300

    Добавьте файлы проекта.

---
+++ WorkWithFiles_8_6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkWithFiles_8_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
